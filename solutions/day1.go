4236104ad7ddc1642210115a31508cdb3d2bbc9f
FabianHartman <fabian.hartman@student.hu.nl>
2024-12-01 09:11:54 +0100

    day 1b

--- solutions/day1.go
+++ solutions/day1.go
@@ -10,8 +10,10 @@ import (
 	"strings"
 )
 
-func Day1() error {
-	file, err := os.Open("inputs/day1.txt")
+var inputPath string = "inputs/day1.txt"
+
+func Day1a() error {
+	file, err := os.Open(inputPath)
 	if err != nil {
 		return fmt.Errorf("error opening file: %s", err)
 	}
@@ -46,7 +48,46 @@ func Day1() error {
 		total += int(math.Abs(float64(firstList[i] - secondList[i])))
 	}
 
-	fmt.Println(total)
+	fmt.Println("Day 1a:", total)
+
+	return nil
+}
+
+func Day1b() error {
+	file, err := os.Open(inputPath)
+	if err != nil {
+		return fmt.Errorf("error opening file: %s", err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	firstList := []int{}
+	secondCounts := map[int]int{}
+
+	for scanner.Scan() {
+		cols := strings.Split(scanner.Text(), "   ")
+
+		first, _ := strconv.Atoi(cols[0])
+		second, _ := strconv.Atoi(cols[1])
+
+		firstList = append(firstList, first)
+		secondCounts[second]++
+	}
+
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("error reading the file: %s", err)
+	}
+
+	total := 0
+
+	for i := 0; i < len(firstList); i++ {
+		number := firstList[i]
+		total += secondCounts[number] * number
+	}
+
+	fmt.Println("Day 1b:", total)
 
 	return nil
 }
