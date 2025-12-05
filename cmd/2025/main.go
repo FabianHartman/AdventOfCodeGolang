97417a8505941341b029a06fc32af837701be58b
fabian <fabian.hartman@student.hu.nl>
2025-12-05 11:32:33 +0100

    day5a

--- cmd/2025/main.go
+++ cmd/2025/main.go
@@ -6,6 +6,7 @@ import (
 	"adventOfCode/solutions/2025/day2"
 	"adventOfCode/solutions/2025/day3"
 	"adventOfCode/solutions/2025/day4"
+	"adventOfCode/solutions/2025/day5"
 )
 
 func main() {
@@ -17,4 +18,5 @@ func main() {
 	runner.Run(day3.Part2)
 	runner.Run(day4.Part1)
 	runner.Run(day4.Part2)
+	runner.Run(day5.Part1)
 }
---
+++ solutions/2025/day5/day5.go
@@ -0,0 +1,98 @@
+package day5
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const inputPath = "inputs/2025/day5.txt"
+
+type IngredientID int
+
+type Range struct {
+	Begin IngredientID
+	End   IngredientID
+}
+
+func Part1() error {
+	var result int
+
+	file, err := os.Open(inputPath)
+	if err != nil {
+		return fmt.Errorf("error opening file: %s", err)
+	}
+
+	defer func(file *os.File) {
+		err = file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+
+	var ranges []Range
+
+	for scanner.Scan() {
+		inputRow := scanner.Text()
+
+		if inputRow == "" {
+			break
+		}
+
+		rangeParts := strings.Split(inputRow, "-")
+		if len(rangeParts) != 2 {
+			return fmt.Errorf("invalid input for range: %s", inputRow)
+		}
+
+		begin, err := strconv.Atoi(rangeParts[0])
+		if err != nil {
+			return fmt.Errorf("invalid input for range: %s", rangeParts[0])
+		}
+
+		end, err := strconv.Atoi(rangeParts[1])
+		if err != nil {
+			return fmt.Errorf("invalid input for range: %s", rangeParts[1])
+		}
+
+		ranges = append(ranges, Range{
+			Begin: IngredientID(begin),
+			End:   IngredientID(end),
+		})
+	}
+
+	var ingredients []IngredientID
+
+	for scanner.Scan() {
+		inputRow := scanner.Text()
+
+		id, err := strconv.Atoi(inputRow)
+		if err != nil {
+			return fmt.Errorf("invalid ingredient ID: %s", inputRow)
+		}
+
+		ingredients = append(ingredients, IngredientID(id))
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		return fmt.Errorf("error reading the file: %s", err)
+	}
+
+	for _, ingredient := range ingredients {
+		for _, ingredientRange := range ranges {
+			if ingredient >= ingredientRange.Begin && ingredient <= ingredientRange.End {
+				result++
+
+				break
+			}
+		}
+	}
+
+	fmt.Println("Day 5a:", result)
+
+	return nil
+}
